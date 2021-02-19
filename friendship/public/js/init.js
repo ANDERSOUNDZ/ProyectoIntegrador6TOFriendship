62091e80d0f927f598a649b30a75e5b5c4e53755
Anderson Chanchay <andersonchanchay@iMac-de-Anderson.local>
2021-02-19 11:37:22 -0500

    actualizano dpooryetco

--- friendship/public/js/init.js
+++ friendship/public/js/init.js
@@ -9,6 +9,7 @@ PIXI.utils.sayHello(type)
 var heightWindow = window.innerHeight;
 var width = 700;
 
+//Creando Variables para Pixi, Usuario y enemigos
 let Application = PIXI.Application,
     loader = PIXI.loader,
     resources = PIXI.loader.resources,
@@ -17,35 +18,94 @@ let Application = PIXI.Application,
     Graphics = PIXI.Graphics,
     renderer = PIXI.autoDetectRenderer(width, heightWindow);
 
+let principal;
+let enemigos = [];
+let velocidadGeneraEnemigo = 100;
+let cuentaInfinita = 0;
+let velocidadEnemigo = 1;
+let velocidadPrincipal =4;
+let velocidaEstandarEnemigo = 1;
+
+//Dando forma a el juego
 let game = new Application({ width:width, height:heightWindow });
 game.renderer.backgroundColor = 0x061639;
 game.renderer.autoRezise = true;
 
+//Devolviendo a la vista HTML
 document.getElementById("juego").appendChild(game.view);
 
-//Creando enemigoss
+//Configuracion Jugador
 setup();
-
 function setup(delta) {
-    //Creando jugador
-    var anchoPersonaje = 64;
-    var altoPersonaje = 64;
-    var posicionX = (renderer.width/2)-(anchoPersonaje/2);
-    var posicionY = renderer.height - altoPersonaje;
-    var rectangle = new Graphics();
-    rectangle.lineStyle(4, 0xFF3300, 1);
-    rectangle.beginFill(0x66CCFF);
-    rectangle.drawRect(0, 0, anchoPersonaje, altoPersonaje);
-    rectangle.endFill();
-    rectangle.x = posicionX;
-    rectangle.y = posicionY;
-    rectangle.vx = 0;
-    rectangle.vy = 0;
-    game.stage.addChild(rectangle);
-
+    //Declarando variables de teclado
+    let left = keyboard("ArrowLeft"), 
+        right = keyboard("ArrowRight");
+        down = keyboard("ArrowDown");
+    //Creando y configurando jugador
+    principal = jugador();
+    game.stage.addChild(principal);
+    //Funciones de reconocimiento de Teclado
+    left.press = () => { 
+        principal.vx = -velocidadPrincipal;
+        principal.vy = 0;
+    }   
+    left.release = () => {
+            principal.vx = 0;
+            principal.vy = 0;
+    }
+    right.press = () => { 
+        principal.vx = velocidadPrincipal;
+        principal.vy = 0;
+    }   
+    right.release = () => {
+            principal.vx = 0;
+            principal.vy = 0;
+    }
+    //Para la velocidad de enemigo se utilizara el down
+    down.press = () => { 
+        velocidadEnemigo += 10;
+        velocidadGeneraEnemigo = 10; 
+    }   
+    down.release = () => {
+        velocidadEnemigo = velocidaEstandarEnemigo;
+        velocidadGeneraEnemigo = 100;;
+    }
+    //
+    state = play;
+    //
     game.ticker.add(delta => gameLoop(delta));
 }
 
+//Creando enemigos aleatorios
 function gameLoop(delta) {
-    //console.log(1);
+    //CreandoEnemigos Aleatorios
+    cuentaInfinita++;
+    if((cuentaInfinita % velocidadGeneraEnemigo)==0){
+        game.stage.addChild(boots());
+    }
+
+    for (let index = 1; index < enemigos.length; index++) {
+        enemigos[index].vy = enemigos[index].vy + velocidadEnemigo; 
+        enemigos[index].y = enemigos[index].vy; 
+    }
+
+    state(delta);
+   
+}
+
+function play (delta){
+    //Reconocimiento de Tecla, perimite conocer el estado al momento de aplastar, va cambiando mediante lo reconoce de izquierda a derecha
+    principal.x += principal.vx;
+    principal.y += principal.vy;
+    
+    //Reconocimiento de choque de enemigo para parar el juego
+    for (let index = 1; index < enemigos.length; index++) {
+        if (hitTestRectangle(enemigos[index], principal)) {            
+            console.log("Enemigo lo golpeo");
+            //game.stop();    
+        }
+        else{
+
+        }
+    }
 }
